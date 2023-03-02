1db990860453257166fb922b7e9b433691225eee
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-02 18:43:47 +0100

    Install prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
@@ -14,4 +14,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
